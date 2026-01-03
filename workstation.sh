3083a39e94bbbb938aa802a992f9f10bc7344886
Siva prasad <sivaprasad3150@gmail.com>
2026-01-03 13:03:24 +0530

    jenkins

--- workstation.sh
+++ workstation.sh
@@ -23,3 +23,8 @@ sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
 sudo curl -sS https://webinstall.dev/k9s | bash
+
+#helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
